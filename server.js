0ca2884b9b7a539b8f02a5da85e4bb6393a9ebb5
Davidson jose <91620722+Davidsonjose@users.noreply.github.com>
2022-03-28 22:41:34 +0100

    done with nodeclass3b

--- server.js
+++ server.js
@@ -1,41 +1,43 @@
-const http = require("http");
-const { data } = require("./data");
-console.log(http.STATUS_CODES);
-const path = require("path");
+const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-  switch (req.url) {
-    case "/":
-      const homePagePath = path.join(__dirname, "views/index.html");
-      const readFilePath = fs.readFileSync(homePagePath, "utf-8")
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write(readFilePath);
-      res.end();
-      break;
-    case "/students":
-      res.writeHead(200, { "content-type": "application/json" });
-      res.write(JSON.stringify(data));
-      res.end();
-      break;
-    case "/users":
-      const userPagePath = path.join(__dirname, "views/user.html");
-      const readuserPath = fs.readFileSync(userPagePath, "utf-8")
-      res.writeHead(200, { "content-type": "text/html" });
-      res.write(readuserPath)
-      res.end();
-      break;
+//initiallized our app using express
+const app = express();
 
-    default:
-      res.writeHead(404, { "content-type": "text/html" });
-      res.write(
-        "<h2>Look like you are lost.... sorry the route you requested was not found or has been moved😎😎😎</h2>"
-      );
-      res.end();
-      break;
-  }
+app.use(express.static(path.join(__dirname, "public")));
+
+//inititalized our body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+app.get("/", (req, res) => {
+  const homePagePath = path.join(__dirname, "views/index.html");
+  const readHomePathContent = fs.readFileSync(homePagePath, "utf-8");
+  res.send(readHomePathContent);
 });
 
-server.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000/");
+app.get("/contact", (req, res) => {
+  const aboutPagePath = path.join(__dirname, "views/contact.html");
+  const readAboutPageContent = fs.readFileSync(aboutPagePath, "utf-8");
+  res.send(readAboutPageContent);
+});
+
+app.get("/user", (req, res) => {
+  const userPagePath = path.join(__dirname, "views/user.html");
+  const readUserPageFile = fs.readFileSync(userPagePath, "utf-8");
+  res.send(readUserPageFile);
+});
+
+app.post("/contact", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+  // res.send(req.body);
+});
+
+const PORT = 10000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
